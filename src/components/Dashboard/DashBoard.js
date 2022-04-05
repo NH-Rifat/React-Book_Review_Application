e9cb4bd49ee223bfaed2f16e5246ee6127274a99
Hasan Rifat <hasanrifatr1@gmail.com>
2022-04-05 17:28:03 +0600

    Charts are successfully show to the Dashboard

--- src/components/Dashboard/DashBoard.js
+++ src/components/Dashboard/DashBoard.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, ComposedChart, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+import './DashBoard.css'
 
 const DashBoard = () => {
+    const [data,setData]=useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setData(data))
+    },[])
     return (
-        <div>
-            <h1>This is Dashboard</h1>
+        <div className="">
+            <div className='composed_chart-lineChart'>
+                <div className="composed_chart">
+                    <h2>Composed chart</h2>
+                    <ComposedChart width={730} height={400} data={data}>
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <CartesianGrid stroke="#f5f5f5" />
+                        <Area type="monotone" dataKey="sell" fill="#8884d8" stroke="#8884d8" />
+                        <Bar dataKey="investment" barSize={28} fill="#413ea0" />
+                        <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+                    </ComposedChart>
+                </div>
+
+                <div className="lineChart">
+                    <h2>Line Chart</h2>
+                    <LineChart width={700} height={450} data={data}
+                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="revenue" stroke="#8884d8" />
+                        <Line type="monotone" dataKey="investment" stroke="#82ca9d" />
+                        </LineChart>
+                </div>
+            </div>
+            
+            <div className="bar_chart_areaChart">
+                <div className="bar_chart">
+                    <h2>Bar Chart</h2>
+                    <BarChart width={730} height={450} data={data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" fill="#8884d8" />
+                        <Bar dataKey="revenue" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+                <div className="area_chart">
+                    <h2>Area Chart</h2>
+                    <AreaChart width={730} height={370} data={data}
+                    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                    <defs>
+                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+                    </linearGradient>
+                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+                    </linearGradient>
+                    </defs>
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                    </AreaChart>
+                </div>
+            </div>
         </div>
     );
 };
